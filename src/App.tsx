1bb4598c1a3306b167f9fe1d371e148addb17981
Varun Potti <potti.varun07@gmail.com>
2022-10-05 10:16:26 +0530

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="w-screen h-screen bg-black"></div>;
+}
+
+export default App;
