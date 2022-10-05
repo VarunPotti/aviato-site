71b225b35a258eeb4cf6ec1dd5ce990b68bb98e6
Varun Potti <potti.varun07@gmail.com>
2022-10-05 13:32:46 +0530

    fix: company selection

--- src/components/body/companies.tsx
+++ src/components/body/companies.tsx
@@ -4,13 +4,13 @@ import { useAtom } from "jotai";
 const Companies = () => {
   const [companies, setCompanies] = useAtom(companiesAtom);
   return (
-    <div className="flex flex-col w-1/4 gap-[20px] overflow-y-auto py-4 select-none">
+    <div className="flex flex-col w-1/4 gap-[20px] overflow-y-auto py-4 ">
       {companies.map((company) => (
         <div className="flex gap-3 items-center justify-start h-12 rounded-full w-full">
           <img
             src={company.logo}
             alt={company.name}
-            className=" rounded-full"
+            className="select-none rounded-full"
           />
           <div className="flex flex-col gap-1">
             <span className="font-medium text-[16px] leading-5">
